1becbf94fe34fcaa72291f67f25e4bfd286714bf
GlebPivanov <112856583+GlebPivanov@users.noreply.github.com>
2023-05-31 18:22:24 +0300

    Create index.php

---
+++ practika/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="styles/index.css">
+    <link type="image/x-icon" href="images/logo.png" rel="shortcut icon">
+    <link type="Image/x-icon" href="images/logo.png" rel="icon">
+    <title>Company</title>
+</head>
+<body>
+    <header>
+        <div class="header-items">
+            <a href="#" class="logo">
+                <img src="images/logo.png" alt="logo" width="37" height="37">
+                <h1>Учёт расхода материалов в Древо Индастрис&nbsp;&nbsp;</h1>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="Sotrudniki.php">Список сотрудников компании</a></li>
+                    <li><a href="Zatrats.php">Список статей затрат</a></li>
+                    <li><a href="Departments.php">Список департаментов</a></li>
+                    <li><a href="Kans.php">Журнал учёта расхода канцтоваров</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="main-item">
+            <a href="Sotrudniki.php">
+                <img src="images/Sotrudniki.png" width="100" height="100" alt="Sotrudniki">
+                <p>Список<br>сотрудников компании</p>
+            </a>
+        </div>
+        <div class="main-item">
+            <a href="Zatrats.php">
+                <img src="images/Zatrats.png" width="157" height="100" alt="Zatrats">
+                <p>Список<br>затрат</p>
+            </a>
+        </div>
+        <div class="main-item">
+            <a href="Departments.php">
+                <img src="images/Departments.png" width="100" height="100" alt="Departments">
+                <p>Список<br>департаментов</p>
+            </a>
+        </div>
+        <div class="main-item">
+            <a href="Kans.php">
+                <img src="images/Kans.png" width="100" height="100" alt="Kans">
+                <p>Журнал учёта<br>расхода канцтоваров</p>
+            </a>
+        </div>
+    </main>
+</body> 
+</html>
